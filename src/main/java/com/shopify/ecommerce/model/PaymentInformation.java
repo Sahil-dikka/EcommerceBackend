6bb7dd1c706710882ca02d2d527363989d24b3bc
Sahil Dikka <sahildikka2001@gmail.com>
2025-10-21 19:00:40 +0530

    added more models

--- src/main/java/com/shopify/ecommerce/model/PaymentInformation.java
+++ src/main/java/com/shopify/ecommerce/model/PaymentInformation.java
@@ -1,6 +1,27 @@
 package com.shopify.ecommerce.model;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
-@Entity
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
 public class PaymentInformation {
+
+    @Column(name = "cardholder_name")
+    private String cardHolderName;
+
+
+    @Column(name = "card_number")
+    private String cardNumber;
+
+    @Column(name = "expiration_date")
+    private LocalDate expriationDate;
+
+    @Column(name = "cvv")
+    private String cvv;
+
+
+
 }
--- src/main/java/com/shopify/ecommerce/model/Review.java
+++ src/main/java/com/shopify/ecommerce/model/Review.java
@@ -1,6 +1,80 @@
 package com.shopify.ecommerce.model;
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String review;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    @JsonIgnore
+    private Product product;
+
+
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public Review(Long id, String review, User user, Product product, LocalDateTime createdAt) {
+        this.id = id;
+        this.review = review;
+        this.user = user;
+        this.product = product;
+        this.createdAt = createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Review(){
+
+    }
 }
